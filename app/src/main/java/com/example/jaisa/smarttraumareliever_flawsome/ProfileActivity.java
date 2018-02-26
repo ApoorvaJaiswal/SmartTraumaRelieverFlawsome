f5dc0a37ceb5041e3ac6b9c78e3f7e2ac62eb40e
ApoorvaJaiswal <jais.appy19960407@gmail.com>
2018-02-26 17:12:24 +0530

    ProfileActivity+Layout added. Trigger it from home

---
+++ app/src/main/java/com/example/jaisa/smarttraumareliever_flawsome/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.jaisa.smarttraumareliever_flawsome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+    ImageView im1,im2;
+    TextView t1,t2,t3,t4,t5,t6,t7,t8,t9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        im1= findViewById(R.id.profileIcon);
+        im2= findViewById(R.id.trackImage);
+        t1= findViewById(R.id.name);
+        t2= findViewById(R.id.nameValue);
+        t3= findViewById(R.id.phone);
+        t4= findViewById(R.id.phoneValue);
+        t5= findViewById(R.id.reportedCrimeNumber);
+        t6= findViewById(R.id.reportedCrimeText);
+        t7= findViewById(R.id.solvedCrimeNumber);
+        t8= findViewById(R.id.solvedCrimeText);
+        t9= findViewById(R.id.trackCrimeText);
+
+    }
+}
