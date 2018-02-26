e2a94d661e01f5c7146bcf32b9b8a944288fbdde
VarshaJambunathan <jvarsha621@gmail.com>
2018-02-27 01:18:08 +0530

    Home Screen UI Completed for English language.

--- app/src/main/java/com/example/jaisa/smarttraumareliever_flawsome/MainActivity.java
+++ app/src/main/java/com/example/jaisa/smarttraumareliever_flawsome/MainActivity.java
@@ -3,22 +3,42 @@ package com.example.jaisa.smarttraumareliever_flawsome;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    Button b;
+
+    LinearLayout reportingStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b= findViewById(R.id.b);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
-            }
-        });
+        getSupportActionBar().setSubtitle(R.string.app_bytext);
+
+        reportingStatus = (LinearLayout)findViewById(R.id.reporting_status);
+        //reportingStatus.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_user_profile){
+            startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
