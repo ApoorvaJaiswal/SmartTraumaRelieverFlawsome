ea357a8f1e5009e1476d5fbf706ed65d504b6ec9
VarshaJambunathan <jvarsha621@gmail.com>
2018-03-11 22:02:02 +0530

    Json parser function

---
+++ app/src/main/java/com/example/jaisa/smarttraumareliever_flawsome/Beans/Complaint.java
@@ -0,0 +1,13 @@
+package com.example.jaisa.smarttraumareliever_flawsome.Beans;
+
+/**
+ * Created by Varsha on 11-03-2018.
+ */
+
+public class Complaint {
+    String subject;
+    String desciption;
+    String date;
+    String time;
+    String reportedTo;
+}
