73b390be2c5fa36702554331359d3771ea4773da
Ashish <ash.95cool@gmail.com>
2018-03-11 17:50:15 +0530

    CrimeDetailsActivitySomePart

---
+++ app/src/main/java/com/example/jaisa/smarttraumareliever_flawsome/DialogAdditionalCommentFragment.java
@@ -0,0 +1,37 @@
+package com.example.jaisa.smarttraumareliever_flawsome;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogAdditionalCommentFragment extends DialogFragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public static DialogAdditionalCommentFragment newInstance(String param1, String param2) {
+        DialogAdditionalCommentFragment fragment = new DialogAdditionalCommentFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_dialog_additional_comment, null);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        return new AlertDialog.Builder(getActivity()).setView(v).create();
+    }
+}
